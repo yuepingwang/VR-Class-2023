657434beb144edf26ba0321b9c79ab536f72da84
yuepingwang <yw1344@nyu.edu>
2023-01-31 17:29:59 -0500

    added globe for boxBot to walk on

--- js/scenes/testRobo.js
+++ js/scenes/testRobo.js
@@ -2,25 +2,23 @@ import { controllerMatrix, buttonState, joyStickState } from "../render/core/con
 
 export const init = async model => {
     let cube = model.add('cube').texture('../media/textures/brick.png');
-    // let tube1 = model.add('tubeX').color(1,0,0);
-    // let tube2 = model.add('tubeY').color(0,1,0);
-    // let tube3 = model.add('tubeZ').color(0,0,1);
-    // let ball = model.add('sphere').color(1,1,0);
 
-    // nested components
-    // let head = model.add();
-    // head.add('tubeY').color(0.4,0.4,0.42);
-    // head.add('tubeY').color(0.4,0.4,0.42);
+    // spacing variables
+    let leg_spacing =0.16;
+    let lense_spacing = 0.4;
+    // size variables
+    let lense_dia = 0.16;
 
-    let leg_spacing =0.1;
     let box = model.add();
     box.add('cube').color(.87,.68,.5).texture('../media/textures/box-side.png');//right side
-    box.add('cube').color(.87,.68,.5).texture('../media/textures/box-front.png');//left side
-    box.add('cube').color(.87,.68,.5).texture('../media/textures/box-side.png');// front side
+    box.add('cube').color(.87,.68,.5).texture('../media/textures/box-front.png');//front side
+    box.add('cube').color(.87,.68,.5).texture('../media/textures/box-side.png');// left side
     box.add('cube').color(.87,.68,.5).texture('../media/textures/box-front.png');//back side
     box.add('cube').color(.87,.68,.5).texture('../media/textures/box-front.png');//bottom
     box.add('cube').color(.87,.68,.5).texture('../media/textures/box-front.png');// flaps
     box.add('cube').color(.87,.68,.5).texture('../media/textures/box-front.png');// flaps
+    box.add('cube').color(0,0,0).texture('../media/textures/black-metal.jpeg');// beard
+    box.add('cube').color(0,0,0).texture('../media/textures/black-metal.jpeg');// beard
 
     let shoe1 = model.add();
     shoe1.add('sphere').color(0.55,0.3,0);
@@ -33,25 +31,40 @@ export const init = async model => {
     shoe2.add('sphere').color(0.55,0.3,0);
 
     let leg1 = model.add();
-    leg1.add('tubeY').color(0.55,0.3,0).texture('../media/textures/tree2.jpeg');
-    leg1.add('tubeY').color(0.55,0.3,0).texture('../media/textures/tree2.jpeg');
-    leg1.add('sphere').color(0.55,0.3,0).texture('../media/textures/tree2.jpeg');
+    leg1.add('tubeY').color(0.55,.4,.2).texture('../media/textures/tree2.jpeg');
+    leg1.add('tubeY').color(0.55,.4,.2).texture('../media/textures/tree2.jpeg');
+    leg1.add('sphere').color(0.55,.4,.2).texture('../media/textures/tree2.jpeg');
     leg1.add(shoe1);
 
     let leg2 = model.add();
-    leg2.add('tubeY').color(0.55,0.3,0).texture('../media/textures/tree2.jpeg');
-    leg2.add('tubeY').color(0.55,0.3,0).texture('../media/textures/tree2.jpeg');
-    leg2.add('sphere').color(0.55,0.3,0).texture('../media/textures/tree2.jpeg');
+    leg2.add('tubeY').color(.55,.4,.2).texture('../media/textures/tree2.jpeg');
+    leg2.add('tubeY').color(.55,.4,.2).texture('../media/textures/tree2.jpeg');
+    leg2.add('sphere').color(.55,.4,.2).texture('../media/textures/tree2.jpeg');
     leg2.add(shoe2);
 
+    let lense = model.add();
+    lense.add('tubeZ').color(.3,.2,.1).texture('../media/textures/black-metal.jpeg');
+    lense.add('tubeZ').color(.3,.2,.1).texture('../media/textures/black-metal.jpeg');
+    lense.add('tubeZ').color(1,1,1).texture('../media/textures/glass1.jpeg');
+    lense.add('tubeZ').color(1,1,1).texture('../media/textures/glass1.jpeg');
+    lense.add('cube').color(.3,.2,.1).texture('../media/textures/black-metal.jpeg');
 
+    let eye = model.add();
+    eye.add('sphere').color(.2,.7,1).texture('../media/textures/glass1.jpeg');
+    eye.add('sphere').color(.2,.7,1).texture('../media/textures/glass1.jpeg');
+    eye.add('sphere').color(0,0,0);
+    eye.add('sphere').color(0,0,0);
 
-    // let leg2 = model.add();
-    // leg2.add('tubeY').color(0,0,1);
-    // leg2.add('tubeY').color(0,0,1);
+    let globe = model.add();
+    globe.add('sphere').color(.9,1,1).texture('../media/textures/grass1.avif');
+    globe.add('tubeX').color(1,1,1).texture('../media/textures/road2.png');
+
 
     let hat = model.add();
-    hat.add('sphere').color(.3,.59,.8).texture('../media/textures/tweed.jpeg').identity().scale(.1,0.3,0.3);
+    hat.add('sphere').color(.3,.69,1).texture('../media/textures/tweed.jpeg');
+    hat.add('sphere').color(.3,.69,1).texture('../media/textures/tweed.jpeg');
+    hat.add('tubeY').color(.3,.69,1).texture('../media/textures/tweed.jpeg');
+    hat.add('sphere').color(0.96, 0.29, 0.54).texture('../media/textures/pink-fur.jpeg');
 
     model.move(0,1.5,0).scale(.3).animate(() => {
         cube.identity().move(Math.sin(model.time),4,0)
@@ -69,34 +82,56 @@ export const init = async model => {
         box.child(4).identity().move(0,-.4,0).scale(.58,.1,.58);
         box.child(5).identity().move(-.83,.35,0).turnZ(0.5).scale(.3,.01,.3);
         box.child(6).identity().move(.83,.35,0).turnZ(-0.5).scale(.3,.01,.3);
+        box.child(7).identity().move(-.1,-.16,.54).turnZ(.3).turnX(-0.6).scale(.08,.06,.06);
+        box.child(8).identity().move(.1,-.16,.54).turnZ(-.3).turnX(-0.6).scale(.08,.06,.06);
 
         // leg left
         shoe1.child(0).identity().move(Math.sin(3*model.time-.5)/5 +.34,-1.1,-leg_spacing).scale(.15,0.05,0.1);
         shoe1.child(1).identity().move(Math.sin(3*model.time-.5)/5 +.28,-1.1,-leg_spacing).scale(.12,0.05,0.1);
         shoe1.child(2).identity().move(Math.sin(3*model.time-.5)/5 +.29,-1.05,-leg_spacing).scale(.09);
-        shoe1.identity().turnZ(Math.sin(3*model.time-.5)/2.2 -0.39);
+        shoe1.identity().turnY(-Math.PI/2).turnZ(Math.sin(3*model.time-.5)/2.2 -0.39);
 
         leg1.child(0).identity().move(0,-.6,-leg_spacing).scale(.1,.15,.1);
         leg1.child(1).identity().move(Math.sin(3*model.time-.5)/15 -.08,-.94,-leg_spacing).turnZ(Math.sin(3*model.time-.5)/2.2 -0.4).scale(.1,.15,.1);
         leg1.child(2).identity().move(-0.01,-.8,-leg_spacing).turnY(Math.PI/2).turnX(Math.PI/2).scale(.11);
-        leg1.identity().turnZ(Math.sin(3*model.time-.5)/2);
+        leg1.identity().turnY(-Math.PI/2).turnZ(Math.sin(3*model.time-.5)/2);
 
         // leg right
         shoe2.child(0).identity().move(Math.sin(3*model.time-.5)/5 +.34,-1.1,leg_spacing).scale(.15,0.05,0.1);
         shoe2.child(1).identity().move(Math.sin(3*model.time-.5)/5 +.28,-1.1,leg_spacing).scale(.12,0.05,0.1);
         shoe2.child(2).identity().move(Math.sin(3*model.time-.5)/5 +.29,-1.05,leg_spacing).scale(.09);
-        shoe2.identity().turnZ(Math.sin(3*model.time+1.7)/1.4 -0.39);
+        shoe2.identity().turnY(-Math.PI/2).turnZ(Math.sin(3*model.time+1.7)/1.4 -0.39);
+
 
         leg2.child(0).identity().move(0,-.6,leg_spacing).scale(.1,.15,.1);
         leg2.child(1).identity().move(Math.sin(3*model.time+1.5)/15 -.08,-.94,leg_spacing).turnZ(Math.sin(3*model.time+1.5)/2.2 -0.4).scale(.1,.15,.1);
         leg2.child(2).identity().move(-0.01,-.8,leg_spacing).turnY(Math.PI/2).turnX(Math.PI/2).scale(.11);
-        leg2.identity().turnZ(Math.sin(3*model.time+1.5)/2);
+        leg2.identity().turnY(-Math.PI/2).turnZ(Math.sin(3*model.time+1.5)/2);
 
 
         // shoe1.identity().move(Math.sin(model.time-.5)/3 -.08,-.94,0).turnZ(Math.sin(model.time-.5)/2.2).turnZ(Math.sin(model.time-.5)/2);
+        hat.child(0).identity().move(-.5,.64,.52).turnZ(-1.5).turnY(1.8).scale(.14,0.16,0.16);
+        hat.child(1).identity().move(-.5,.77,.52).turnZ(-1.5).turnY(1.8).scale(.1,0.12,0.12);
+        hat.child(2).identity().move(-.5,.55,.52).turnZ(.05).turnX(-.1).scale(.18,.05,.18);
+        hat.child(3).identity().move(-.5,.92,.52).turnZ(.05).turnY(-.5).scale(.06,.06,.06);
+        hat.identity().turnZ(.2).move(.13,.1,-.3);
 
-        hat.identity().move(0,.5,0).turnZ(-1.3).turnY(-0.3);
-        hat.identity().move(0,.5,0).turnZ(-1.3).turnY(-0.3);
+        lense.child(0).identity().move(-lense_spacing/2,.1,.59).scale(lense_dia,lense_dia,.01);
+        lense.child(1).identity().move(lense_spacing/2,.1,.59).scale(lense_dia,lense_dia,.01);
+        lense.child(2).identity().move(-lense_spacing/2,.1,.59).scale(lense_dia-.02,lense_dia-.02,.011);
+        lense.child(3).identity().move(lense_spacing/2,.1,.59).scale(lense_dia-.02,lense_dia-.02,.011);
+        lense.child(4).identity().move(0,.1,.59).scale(lense_spacing/2-lense_dia+.01,.016,.01);
+
+        eye.child(0).identity().move(-lense_spacing/2,.1,.6).scale(.06,.06,.011);
+        eye.child(1).identity().move(lense_spacing/2,.1,.6).scale(.06,.06,.011);
+        eye.child(2).identity().move(-lense_spacing/2,.1,.6).scale(.05,.05,.014);
+        eye.child(3).identity().move(lense_spacing/2,.1,.6).scale(.05,.05,.014);
+        eye.identity().move(Math.sin(1.5*model.time)/20,0,0);
+
+        globe.child(0).identity().turnY(Math.PI/2).scale(4.6);
+        globe.child(1).identity().scale(.7,4.63,4.63);
+        globe.identity().move(0,-5.8,0).turnX(-model.time/4);
+
 
         // head.child(0).identity().scale(0.2,.3,0.2);
         // head.child(1).identity().move(0,-0.4,0).turnX(0.2).scale(0.2,.04,0.2);
